b70a5f6d8cc149ef3ff8e6544f002cc3c5b1d843
Alan <alan.moment77@gmail.com>
2018-04-12 19:23:07 +0800

    how to show error msg

--- cc_iaas_sdnms/resources/base_resource.py
+++ cc_iaas_sdnms/resources/base_resource.py
@@ -2,16 +2,3 @@ class BaseResource(object):
 
     def __init__(self, db_manager):
         self.db = db_manager
-        self.result = {
-            "status": 0,
-            "error_msg": "",
-            "content": {}
-        }
-
-    def set_result(self, status, error_msg, content):
-        self.result["status"] = status
-        self.result["error_msg"] = error_msg
-        self.result["content"] = content
-
-    def get_result(self):
-        return self.result
