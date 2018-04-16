f108c303e8abc02671f4845f3ab89da9f03fc746
robin <robin@cloudcube.com.tw>
2018-04-16 16:53:22 +0800

    Add config group and option

--- sdnms_api/app/server.py
+++ sdnms_api/app/server.py
@@ -1,8 +1,8 @@
-import os
 import falcon
+import os
+import sys
 
 from oslo_log import log
-
 from sdnms_api.models.manager import DBManager
 from sdnms_api.utils import simport
 from sdnms_api.resources import health
@@ -11,12 +11,11 @@ from sdnms_api import config
 LOG = log.getLogger(__name__)
 CONF = config.CONF
 
-
-def launch(config_file):
-    #config_file = conf.get('config_file', "/etc/monasca/api-config.conf")
-    config.init(config_file=config_file)
+def launch(config_file=None):
     log.set_defaults()
     log.register_options(CONF)
+
+    config.init(args=sys.argv[1:])
 
     mgr = None
     #mgr = DBManager(CONF.database.url)
@@ -30,14 +29,7 @@ def launch(config_file):
     return app
 
 def get_wsgi_app(config_base_path=None, **kwargs):
-    config_file = kwargs.get('config', 'sdnms_api.ini')
-
-    if config_base_path is None:
-        config_base_path = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), '../../etc/sdnms_api')
-
-    config_file = os.path.join(config_base_path, config_file)
-    return launch(config_file)
+    return launch()
 
 if __name__ == '__main__':
     from wsgiref import simple_server
--- sdnms_api/config.py
+++ sdnms_api/config.py
@@ -2,12 +2,32 @@ from oslo_config import cfg
 
 CONF = cfg.CONF
 
+default_opts = [
+    cfg.StrOpt('name',
+                default='cc_iaas_sdnms',
+                help='Process name.'),
+]
+
+database_opts = [
+    cfg.StrOpt('address',
+                default='127.0.0.1',
+                help='Binding address'),
+    cfg.IntOpt('port', default=3306,
+               help='The database port where listen on'),
+    cfg.StrOpt('username', default='root',
+               help='The database user of login'),
+    cfg.StrOpt('password', default='root',
+               help='The database password of login'),
+]
+database_group = cfg.OptGroup(name='database', title='database')
+
 dispatcher_opts = [
     cfg.StrOpt('health',
                default='sdnms_api.resources.health:HealthResource',
                help='HealthResource controller'),
 ]
 dispatcher_group = cfg.OptGroup(name='dispatcher', title='dispatcher')
+
 
 def init(args=[], config_file=None):
     cfg.CONF(args=args,
@@ -16,5 +36,8 @@ def init(args=[], config_file=None):
          default_config_files=[config_file],
          description='SDNMS RESTful API')
 
+    cfg.CONF.register_opts(default_opts)
+    cfg.CONF.register_group(database_group)
+    cfg.CONF.register_opts(database_opts, database_group)
     cfg.CONF.register_group(dispatcher_group)
     cfg.CONF.register_opts(dispatcher_opts, dispatcher_group)
