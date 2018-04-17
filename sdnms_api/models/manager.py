d88db0db669c67df514bc30afafd2aa03f5b516f
Tony Cheng <tonycheng@cloudcube.com.tw>
2018-04-18 00:23:25 +0800

    fix sdnms package path in models

--- sdnms_api/models/manager.py
+++ sdnms_api/models/manager.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoping
 
 # Needed by the setup method as we want to make sure
 # all models are loaded before we call create_all(...)
-from cc_iaas_sdnms.models import models
+from sdnms_api.models import models
 
 
 class DBManager(object):
