57c2d5595b0e5463fdf40689497758594caf2270
Tony Cheng <tony.pig@gmail.com>
2018-04-18 15:24:56 +0800

    add loading config function

--- fw_fortinet_v5_6_3/driver.py
+++ fw_fortinet_v5_6_3/driver.py
@@ -3,54 +3,47 @@ from oslo_config import cfg
 
 class Driver(object):
 
-    @staticmethod
-    def __init__(self, config_file):
+    def __init__(self, config_file=None):
 
         if config_file is None:
             config_file = '/etc/sdnms_api/backends/fw_fortinet_v5.6.3.ini'
 
-        cfg.CONF(project='FortiOS',
-                 version="5.6.3",
-                 default_config_files=[config_file],
-                 description='FortiOS RESTful API')
-
-        CONF = cfg.CONF
+        cfg.CONF(args=[],
+                project='FortiOS',
+                version="5.6.3",
+                default_config_files=[config_file],
+                description='FortiOS RESTful API')
 
         ftg_opts = [
             cfg.StrOpt('http_scheme',
                        default='https',
                        help='http or https protocol for restful api'),
             cfg.StrOpt('http_host',
-                       required=True,
                        help='backend IP which provides restful api endpoint'),
             cfg.PortOpt('http_port',
                         default=443,
                         help='The backend port where listen on'),
             cfg.StrOpt('http_account',
-                       required=True,
                        help='The backend restful api login account'),
             cfg.StrOpt('http_password',
-                       required=True,
                        help='The backend password for restful api login'),
             cfg.StrOpt('ssh_host',
-                       required=True,
                        help='backend IP which provides CLI endpoint'),
             cfg.PortOpt('ssh_port',
                         default=22,
                         help='The backend port where ssh listen on'),
             cfg.StrOpt('ssh_account',
-                       required=True,
                        help='The backend ssh login account'),
             cfg.StrOpt('ssh_password',
-                       required=True,
                        help='The backend password for ssh login'),
         ]
         ftg_group = cfg.OptGroup(name='fw0001', title='ftg')
 
         cfg.CONF.register_group(ftg_group)
-        cfg.CONF.register_opts(ftg_opts)
+        cfg.CONF.register_opts(ftg_opts, ftg_group)
 
-        fw0001_http_scheme = CONF.fw0001.http_scheme
+        print('%s' % (cfg.CONF.fw0001.http_scheme))
+        print('%s' % (cfg.CONF.fw0001.http_host))
+        print('%s' % (cfg.CONF.fw0001.http_port))
 
-
-
+    def query(self, ):
