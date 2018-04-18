b409c2c6200d39905a9a7c9db4972f6ca7a9e916
Tony Cheng <tony.pig@gmail.com>
2018-04-18 17:39:26 +0800

    add 'use' func for fw_fortinet_v5.6.3

--- fw_fortinet_v5_6_3/driver.py
+++ fw_fortinet_v5_6_3/driver.py
@@ -1,5 +1,5 @@
 from oslo_config import cfg
-
+import pdb
 
 class Driver(object):
 
@@ -13,6 +13,27 @@ class Driver(object):
                 version="5.6.3",
                 default_config_files=[config_file],
                 description='FortiOS RESTful API')
+
+        fwid_opts = [
+            cfg.StrOpt('fw',
+                       help='firewalls list'),
+            ]
+
+        fwid_group = cfg.OptGroup(name='identities', title='ftg')
+        cfg.CONF.register_group(fwid_group)
+        cfg.CONF.register_opts(fwid_opts, fwid_group)
+        fw_identities_str = cfg.CONF.identities.fw
+        fw_identities = [x for x in fw_identities_str.split(',')]
+        self.fw_identities = fw_identities
+        #self.use(1)
+
+
+    def use(self, index):
+        self._index = index
+
+
+        print('I am using index:{0} , id:{1}'.format(
+            index, self.fw_identities[index]))
 
         ftg_opts = [
             cfg.StrOpt('http_scheme',
@@ -37,13 +58,11 @@ class Driver(object):
             cfg.StrOpt('ssh_password',
                        help='The backend password for ssh login'),
         ]
-        ftg_group = cfg.OptGroup(name='fw0001', title='ftg')
+        ftg_group = cfg.OptGroup(name=self.fw_identities[index], title='ftg')
 
         cfg.CONF.register_group(ftg_group)
         cfg.CONF.register_opts(ftg_opts, ftg_group)
 
-        print('%s' % (cfg.CONF.fw0001.http_scheme))
-        print('%s' % (cfg.CONF.fw0001.http_host))
-        print('%s' % (cfg.CONF.fw0001.http_port))
-
-    def query(self, ):
+        print('%s' % (cfg.CONF.get(self.fw_identities[index]).http_scheme))
+        print('%s' % (cfg.CONF.get(self.fw_identities[index]).http_host))
+        print('%s' % (cfg.CONF.get(self.fw_identities[index]).http_port))
