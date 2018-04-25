f4474458667eceaf648e40cdd1ea333d4fc43628
TonyCheng <tonycheng@cloudcube.com.tw>
2018-04-26 00:15:38 +0800

    add ignore to virtualenv

--- sdnms_api/app/server.py
+++ sdnms_api/app/server.py
@@ -8,7 +8,6 @@ from sdnms_api.utils import simport
 from sdnms_api.resources import health
 from sdnms_api.resources import firewall
 from sdnms_api import config
-from sdnms_api.driver import loader
 
 LOG = log.getLogger(__name__)
 CONF = config.CONF
@@ -22,7 +21,6 @@ def launch(config_file=None):
     log.register_options(CONF)
 
     config.init(config_file=config_file)
-    loader.setup(CONF)
 
     db = "mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(
             CONF.database.username, CONF.database.password,
@@ -35,8 +33,6 @@ def launch(config_file=None):
     app.add_route("/health", health)
     firewall_address = simport.load(CONF.dispatcher.firewall_address)(mgr)
     app.add_route("/firewall/address", firewall_address)
-    firewall_device = simport.load(CONF.dispatcher.firewall_device)(mgr)
-    app.add_route("/firewall/device", firewall_device)
 
     LOG.debug('Dispatcher drivers have been added to the routes!')
     return app
